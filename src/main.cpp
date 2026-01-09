b60119cdcaf516293e7e175b351487801961da94
Ikhlas RABEH <ikhlasrabeh@gmail.com>
2026-01-09 13:34:33 +0100

    MQTT connection message

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,11 @@
 #include <DHT_U.h>
 #define DHTTYPE    DHT22
 #include <WiFi.h>
-#include <WiFiClientSecure.h>
-#include <PubSubClient.h>
+#include <AsyncMqttClient.h>
+AsyncMqttClient mqttClient;
+
+const char* mqtt_server = "test.mosquitto.org";
+const int mqtt_port = 1883;
 
 const char* ssid = "Wokwi-GUEST";  
 const char* password = "";          
@@ -16,40 +19,20 @@ const int potentio = 35;
 const int dhtPin = 17;
 const int LDR = 32;
 
-DHT_Unified dht(17, DHT22);
+DHT_Unified dht(dhtPin, DHTTYPE);
 
-WiFiClientSecure espClient;        
-PubSubClient client(espClient); 
-
-const char* mqtt_server = "601c0121dc1841cf81e5c9dfb83600d9.s1.eu.hivemq.cloud";
-const int mqtt_port = 8883;
-
-const char* mqtt_user = "ikhlas_iot";
-const char* mqtt_pass = "Robotics_esp32";
-
-void reconnect() {
-  while (!client.connected()) {
-    Serial.print("Connecting to MQTT...");
-    String clientId = "ESP32_" + String(random(0xffff), HEX); // unique ID
-    if (client.connect(clientId.c_str(), mqtt_user, mqtt_pass)) {
-      Serial.println("connected");
-      client.subscribe("mini-serre/#");
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      delay(5000);
-    }
-  }
+void onMqttConnect(bool sessionPresent) {
+    Serial.println("MQTT connection established!");
+    Serial.println("ESP32 is online and ready to publish data.");
+    mqttClient.publish("mini-serre/status", 1, true, "online");
 }
 
 void setup() {
   Serial.begin(115200);
   delay(1000);
-  
+  //wifi setup
   Serial.println("Connecting to WiFi...");
   WiFi.begin(ssid, password);
-
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
@@ -58,45 +41,50 @@ void setup() {
   Serial.println("WiFi connected!");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
-
-  espClient.setInsecure(); // Accept HiveMQ Cloud TLS certificate
-  client.setServer(mqtt_server, mqtt_port);
-
+  //mqtt setup
+  mqttClient.setServer(mqtt_server, mqtt_port);
+  mqttClient.setWill("mini-serre/status", 1, true, "offline"); //last will & testament
+  mqttClient.onConnect(onMqttConnect);
+  mqttClient.connect();
+  //pin config
   pinMode(lampe, OUTPUT);
   pinMode(eau, OUTPUT);
-
   pinMode(potentio, INPUT);
   pinMode(LDR, INPUT);
   dht.begin();
 }
 
 void loop() {
-  if (!client.connected()) {
-    reconnect();
-  }
-  client.loop(); // maintain MQTT connection
-
-  delay(5000);
-
+static unsigned long lastPublish = 0;
+const unsigned long interval = 5000;
+//loop for reading and publishing data
+if (millis() - lastPublish >= interval) { 
+  lastPublish = millis();
+  //reading and converting potentio values
   int hum_sol = analogRead(potentio); 
   int hum_percent = map(hum_sol, 0, 4095, 0, 100);
-  Serial.print("Soil Humidity Percentage: " + String(hum_percent) + "%");
-
+  //reading luminosity value
+  int luminosite = analogRead(LDR);
+  //get temp and hum and verify if there is an error
   sensors_event_t tempEvent, humEvent;
   dht.temperature().getEvent(&tempEvent);
   dht.humidity().getEvent(&humEvent);
+ if (isnan(tempEvent.temperature) || isnan(humEvent.relative_humidity)) {
+    Serial.println("DHT22 error reading data.");
+    return;
+ }
+ if (mqttClient.connected()) {
+  //print to console
+  Serial.print("Soil Humidity Percentage: " + String(hum_percent) + "%");
   Serial.print("Temperature: " + String(tempEvent.temperature, 2) + "°C\t");
   Serial.print("Humidity: " + String(humEvent.relative_humidity, 1) + "%\t");
-
-  int luminosite = analogRead(LDR);
   Serial.println("Luminosity: " + String(luminosite) + "\t");
-
-  // Publishing corrected topics (no spaces)
-  client.publish("mini-serre/soil", String(hum_percent).c_str());
-  client.publish("mini-serre/temperature", String(tempEvent.temperature, 2).c_str());
-  client.publish("mini-serre/humidity", String(humEvent.relative_humidity, 1).c_str());
-  client.publish("mini-serre/light", String(luminosite).c_str());
-
+  //publish to broker
+  mqttClient.publish("mini-serre/soil", 0, false, String(hum_percent).c_str());
+  mqttClient.publish("mini-serre/temperature", 0, false, String(tempEvent.temperature).c_str());
+  mqttClient.publish("mini-serre/humidity", 0, false, String(humEvent.relative_humidity).c_str());
+  mqttClient.publish("mini-serre/light", 0, false, String(luminosite).c_str());
+  //watering manipulation
   if (hum_percent < 40) {
     digitalWrite(eau, HIGH);
     Serial.println("Watering the plant...");
@@ -107,7 +95,7 @@ void loop() {
     digitalWrite(eau, LOW);
     Serial.println("Soil moisture is sufficient.");
   }
-
+  //lamp manipulation
   if (luminosite < 2000) {
     digitalWrite(lampe, HIGH);
     Serial.println("It's dark, turning on the lamp.");
@@ -115,11 +103,15 @@ void loop() {
     digitalWrite(lampe, LOW);
     Serial.println("Light level is sufficient.");
   }
-
+  //temp and hum alerts
   if (tempEvent.temperature > 40) {
     Serial.println("Warning: High temperature detected!");
+    mqttClient.publish("mini-serre/alert", 1, true, "Warning! High temperature detected!");
   }
   if (humEvent.relative_humidity > 80) {
     Serial.println("Warning: High humidity detected!");
+    mqttClient.publish("mini-serre/alert", 1, true, "Warning! High Humidity level detected!");
   }
+}
+}
 }
